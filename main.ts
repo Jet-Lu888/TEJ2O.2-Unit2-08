0ed08f7a2f338fc65a7ac5f81d321f3a66c900a0
Jet-Lu888 <jet.lu@stu.ocsb.ca>
2026-03-05 16:20:06 -0500

    Created the program

--- main.ts
+++ main.ts
@@ -1,8 +1,77 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Jet Lu
+ * Created on: Mar 2026
+ * This program will control an RGB LED.
 */
 
-basic.showString('Hello, World!')
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+pause(500)
+pins.digitalWritePin(DigitalPin.P13, 0) // blue
+pins.digitalWritePin(DigitalPin.P14, 0) // green
+pins.digitalWritePin(DigitalPin.P15, 0) // red
+
+// program
+input.onButtonPressed(Button.A, function() {
+    basic.showIcon(IconNames.Yes)
+    pause(500)
+    basic.clearScreen()
+
+    // show red
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.showString("Red")
+    pause(1)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+
+    // show green
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    basic.showString("Green")
+    pause(1)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+
+    // show blue
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    basic.showString("Blue")
+    pause(1)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+
+    // show magenta
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    basic.showString("Magenta")
+    pause(1)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+
+    // show cyan
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    basic.showString("Cyan")
+    pause(1)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+
+    // show yellow
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    basic.showString("Yellow")
+    pause(1)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+
+    // show white
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    basic.showString("White")
+    pause(1)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+
+    //reset
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
