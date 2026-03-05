046050b1c6bfdd6b2d9ea749a43c3ab0dd68e196
Jet-Lu888 <jet.lu@stu.ocsb.ca>
2026-03-05 16:27:53 -0500

    Fixed pause to meet requirements

--- main.ts
+++ main.ts
@@ -16,32 +16,32 @@ pins.digitalWritePin(DigitalPin.P15, 0) // red
 // program
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Yes)
-    pause(500)
+    pause(1000)
     basic.clearScreen()
 
     // show red
     pins.digitalWritePin(DigitalPin.P15, 1)
     basic.showString("Red")
-    pause(500)
+    pause(1000)
     pins.digitalWritePin(DigitalPin.P15, 0)
 
     // show green
     pins.digitalWritePin(DigitalPin.P14, 1)
     basic.showString("Green")
-    pause(500)
+    pause(1000)
     pins.digitalWritePin(DigitalPin.P14, 0)
 
     // show blue
     pins.digitalWritePin(DigitalPin.P13, 1)
     basic.showString("Blue")
-    pause(500)
+    pause(1000)
     pins.digitalWritePin(DigitalPin.P13, 0)
 
     // show magenta
     pins.digitalWritePin(DigitalPin.P15, 1)
     pins.digitalWritePin(DigitalPin.P13, 1)
     basic.showString("Magenta")
-    pause(500)
+    pause(1000)
     pins.digitalWritePin(DigitalPin.P15, 0)
     pins.digitalWritePin(DigitalPin.P13, 0)
 
@@ -49,7 +49,7 @@ input.onButtonPressed(Button.A, function () {
     pins.digitalWritePin(DigitalPin.P14, 1)
     pins.digitalWritePin(DigitalPin.P13, 1)
     basic.showString("Cyan")
-    pause(500)
+    pause(1000)
     pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P13, 0)
 
@@ -57,7 +57,7 @@ input.onButtonPressed(Button.A, function () {
     pins.digitalWritePin(DigitalPin.P15, 1)
     pins.digitalWritePin(DigitalPin.P14, 1)
     basic.showString("Yellow")
-    pause(500)
+    pause(1000)
     pins.digitalWritePin(DigitalPin.P15, 0)
     pins.digitalWritePin(DigitalPin.P14, 0)
 
@@ -66,7 +66,7 @@ input.onButtonPressed(Button.A, function () {
     pins.digitalWritePin(DigitalPin.P14, 1)
     pins.digitalWritePin(DigitalPin.P13, 1)
     basic.showString("White")
-    pause(500)
+    pause(1000)
     pins.digitalWritePin(DigitalPin.P15, 0)
     pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P13, 0)
